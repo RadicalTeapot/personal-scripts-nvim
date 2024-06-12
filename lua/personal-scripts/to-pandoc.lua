1e71226a3f68a88d237c9154e4abe56268acdd96
Mathias Capdet <mathias.capdet@tescan.com>
2024-06-12 10:35:36 +0200

    Update pandoc string cleaning

--- lua/personal-scripts/to-pandoc.lua
+++ lua/personal-scripts/to-pandoc.lua
@@ -1,5 +1,23 @@
 local M = {}
 
+M.pandocStringSubstitution = {
+    ["\\%["] = "[",
+    ["\\%]"] = "]",
+    ["‘"] = "'",
+    ["’"] = "'",
+    ["“"] = [["]],
+    ["”"] = [["]],
+    ["\\%*"] = "*",
+    ["\\>"] = ">"
+}
+
+M.cleanPandocString = function(input)
+    for pattern, replacement in pairs(M.pandocStringSubstitution) do
+        input = string.gsub(input, pattern, replacement)
+    end
+    return input
+end
+
 M.sendToPandoc = function(opts)
     local start = (opts.line1 or 1) - 1
     local last = opts.line2 or -1
@@ -8,10 +26,7 @@ M.sendToPandoc = function(opts)
     local out = vim.system({"pandoc", "--columns=120", "-t", "gfm"}, {stdin=lines, text=true}):wait()
     assert(out.code == 0, "An error occured "..(out.stderr or ""))
 
-    -- Remove extra pandoc markings
-    local cleaned = string.gsub(out.stdout, "\\%[", "[")
-    cleaned = string.gsub(cleaned, "\\%]", "]")
-
+    local cleaned = M.cleanPandocString(out.stdout)
     local splits = vim.split(cleaned, "\n", {plain=true})
     table.remove(splits) -- pop extra last element
     vim.api.nvim_buf_set_lines(0, start, last, false, splits)
